b1cf698789b41ae604c42a728b3fb0047a45f944
KimYoungSeong <66957218+KimYoungSeong@users.noreply.github.com>
2023-10-25 15:42:13 +0900

    Add files via upload

--- RandomBox.java
+++ RandomBox.java
@@ -1,63 +1,42 @@
-package p1;
+package homework;
 
 import java.util.Random;
+import java.util.Vector;
 
 public class RandomBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a [] = new int[10];
+		Vector<Number> v = new Vector<Number>();
 		Random r = new Random();
-				
+		
 		int i = 0;
-		int j = 0;
+		int d = 0;
 		
-		for(i = 0; i < 4; i++) {
-			a[i] = r.nextInt(7)+1;
-			for(j = 0; j < i; j++) {
-				if(a[i]==a[j]) {
-					i--;
-				}
-				
-		}
-	}
+		Number n1 = new Number("카트");
+		Number n2 = new Number("마크");
+		Number n3 = new Number("카드");
+		Number n4 = new Number("지뢰");
+		Number n5 = new Number("메이플");
+		Number n6 = new Number("로아");
+		Number n7 = new Number("크아");
 		
-		for(i=1;i<=3;i++) {
+		v.add(n1);
+		v.add(n2);
+		v.add(n3);
+		v.add(n4);
+		v.add(n5);
+		v.add(n6);
+		v.add(n7);
+		
+		for(i=0;i<3;i++){
+			d = r.nextInt(v.size());
 			
-			switch(r.nextInt(7)+1) {
-			
-			case 1:{
-				System.out.println("크아");
-				break;
-			}
-			case 2:{
-				System.out.println("카트");
-				break;
-			}
-			case 3:{
-				System.out.println("메이플");
-				break;
-			}
-			case 4:{
-				System.out.println("lol");
-				break;
-			}
-			case 5:{
-				System.out.println("마크");
-				break;
-			}
-			case 6:{
-				System.out.println("카드");
-				break;
-			}
-			case 7:{
-				System.out.println("지뢰");
-				break;
-			}
-			
-			}
+			Number num = v.get(d);
+			System.out.println(num.name);
+			v.remove(d);
 		}
-	
+		System.out.println(" ");
 	}
 
 }
